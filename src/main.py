245fa698b104e1a3de4b544fb4e5a9655387174c
Lev Stambler <levstamb@gmail.com>
2019-06-09 17:16:07 -0400

    overlay and writing file out

--- src/main.py
+++ src/main.py
@@ -1,41 +1,57 @@
 import numpy as np
 import cv2
 
+fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+out = cv2.VideoWriter('output.avi',fourcc, 24, (1920,1080))
 
 def overlay_image(l_img, s_img, x_offset, y_offset):
-    # add alphas
-    l_img = cv2.cvtColor(l_img, cv2.COLOR_RGB2RGBA)
-    s_img = cv2.cvtColor(s_img, cv2.COLOR_RGB2RGBA)
-
     y1, y2 = y_offset, y_offset + s_img.shape[0]
     x1, x2 = x_offset, x_offset + s_img.shape[1]
-    alpha_s = s_img[:, :, 3] / 255.0
-    alpha_l = 1.0 - alpha_s
-    for c in range(0, 3):
-        l_img[y1:y2, x1:x2, c] = (alpha_s * s_img[:, :, c] +
-                                  alpha_l * l_img[y1:y2, x1:x2, c])
+    l_img[y1:y2, x1:x2] = (s_img[:, :] +
+                              l_img[y1:y2, x1:x2])
+    return l_img
 
-def rescaleFrame(frame, factor):
+
+def rescale_frame(frame, factor):
     return cv2.resize(frame, (0, 0), fx=factor, fy=factor, interpolation=cv2.INTER_LINEAR)
 
 
-def main():
-    cap = cv2.VideoCapture('../files/small.mp4')
-    cap2 = cv2.VideoCapture('../files/large.mp4')
+def build_offset(smll_width, smll_height, lrg_width, lrg_height, padding=20):
+    return [
+        lrg_width - smll_width - padding,
+        lrg_height - smll_height - padding
+    ]
 
-    while(cap.isOpened() or cap2.isOpened()):
-        ret, frame = cap.read()
-        ret, frame2 = cap2.read()
-        resized = rescaleFrame(frame, 0.9)
-        overlayed = overlay_image(frame, resized, 50, 50)
-        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        cv2.imshow('frame', frame)
+def build_video(cap_smll, cap_lrg):
+    resize_factor = 0.2
+    smll_width = int(cap_smll.get(3) * resize_factor)
+    smll_height = int(cap_smll.get(4) * resize_factor)
+    lrg_width = int(cap_lrg.get(3))
+    lrg_height = int(cap_lrg.get(4))
+    print(lrg_width, lrg_height)
+    while(cap_smll.isOpened() and cap_lrg.isOpened()):
+        ret, lrg_img = cap_smll.read()
+        ret, smll_img = cap_lrg.read()
+        if lrg_img is None or smll_img is None or ret is not True:
+          break
+        resized = rescale_frame(smll_img, resize_factor)
+        offset = build_offset(smll_width, smll_height, lrg_width, lrg_height)
+        overlayed = overlay_image(lrg_img, resized, *offset)
+        # needs to be converted back to rgb
+        out.write(overlayed)
+        cv2.imshow('frame', overlayed)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
-    cap.release()
+    cap_smll.release()
+    cap_lrg.release()
+    out.release()
     cv2.destroyAllWindows()
 
+
+def main():
+    cap_smll = cv2.VideoCapture('../files/small.mp4')
+    cap_lrg = cv2.VideoCapture('../files/large.mp4')
+    build_video(cap_smll, cap_lrg)
 
 main()
