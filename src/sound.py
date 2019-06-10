49391213d3f204cc455c17afdda3d0a7c088d4e4
Lev Stambler <levstamb@gmail.com>
2019-06-09 22:45:50 -0400

    new audio clip out

--- src/sound.py
+++ src/sound.py
@@ -5,6 +5,17 @@ def convert_clip(path, output_path):
     clip = mp.VideoFileClip(path)
     clip.audio.write_audiofile(output_path)
 
+def speed_change(sound, speed=1.0):
+    # Manually override the frame_rate. This tells the computer how many
+    # samples to play per second
+    sound_with_altered_frame_rate = sound._spawn(sound.raw_data, overrides={
+         "frame_rate": int(sound.frame_rate * speed)
+      })
+     # convert the sound with altered frame rate to a standard frame rate
+     # so that regular playback programs will work right. They often only
+     # know how to play audio at standard frame rate (like 44.1k)
+    return sound_with_altered_frame_rate.set_frame_rate(sound.frame_rate)
+
 def get_quiet_time(sound):
     peak_amplitude = sound.max
     # split sound in 0.1-second slices and export
@@ -25,13 +36,33 @@ def get_quiet_time(sound):
                 quiet_periods_closer.append(i * loop_over_time)
             else:
                 quiet_periods_opener.pop()
-    print(quiet_periods_opener, quiet_periods_closer)
+    return (quiet_periods_opener, quiet_periods_closer)
 
-def get_amplitude():
+def create_new_clip(clip, quiet_opener, quiet_closer):
+    time_start = 100
+    new_clip = clip[:time_start]
+    time_full_end = len(clip)
+    if abs(len(quiet_closer) - len(quiet_opener)) > 1:
+        print(len(quiet_closer), len(quiet_opener))
+        raise ValueError("Opener and closer must be same or one diff in length")
+    for i in range(0, len(quiet_closer)):
+        new_clip += clip[time_start:quiet_opener[i]]
+        if quiet_closer[i] > time_full_end or quiet_opener[i] > time_full_end:
+            break
+        sped_up = speed_change(clip[quiet_opener[i]:quiet_closer[i]], speed=2.0)
+        new_clip += sped_up
+        time_start = quiet_closer[i]
+    if time_full_end is not time_start:
+        new_clip += clip[time_start:time_full_end]
+    return new_clip 
+
+def create_audio_and_quiet_time():
     input_sound = "audiotemp.mp3"
     input_video = "../files/yotubevid1.mp4"
     # convert_clip(input_video, input_sound)
     sound = AudioSegment.from_file("audiotemp.mp3", format="mp3")
-    get_quiet_time(sound)
+    quiet_openers, quiet_closers = get_quiet_time(sound)
+    new_clip = create_new_clip(sound, quiet_openers, quiet_closers)
+    new_clip.export("sped_up.mp3", format="mp3")
 
-get_amplitude()
+create_audio_and_quiet_time()
